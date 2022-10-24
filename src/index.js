87f74a7b3f9dcab06e244439e1dc6bc6ba48dd7e
SeymaDemir <seymademir35@gmail.com>
2022-10-24 15:08:33 +0300

    add-delete task features added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
+
+document.addEventListener("DOMContentLoaded", (e) => {
   // your code here
+  const btnClick = document.querySelector('[type="submit"]')
+  btnClick.addEventListener("click", (e) => {
+    e.preventDefault();
+    //get value from input
+    const newTask = document.getElementById("new-task-description").value;
+    const liElement = document.createElement('li');
+    liElement.textContent = newTask;
+
+    //create a li tag in ul tag and create a button for delete function
+    const taskList = document.getElementById("tasks");
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = "  X";
+    liElement.appendChild(deleteButton);
+    taskList.appendChild(liElement);
+
+    deleteButton.addEventListener('click', deleteTask)
+    
+  })
 });
+
+ function deleteTask(e){
+  e.target.parentNode.remove();
+}
+
